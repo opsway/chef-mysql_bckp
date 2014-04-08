0930d9c611113b9dcc262926b17768b704fbd081
Andriy Samilyak <samilyak@gmail.com>
2014-04-08 17:48:47 +0300

    Dump filename as attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
-default["mysql_bckp"]["local_xml"] = "/var/www/shared/app/etc/local.xml"
-default["mysql_bckp"]["script_path"] = "/var/www/mysql_bckp.rb"
-default["mysql_bckp"]["dump_path"] = "/var/www/db_backups"
+default["mysql_bckp"]["local_xml"] 		= "/var/www/shared/app/etc/local.xml"
+default["mysql_bckp"]["script_path"] 	= "/var/www/mysql_bckp.rb"
+default["mysql_bckp"]["dump_path"] 		= "/var/www/db_backups"
+default["mysql_bckp"]["dump_file"] 		= "dbdump.sql"
