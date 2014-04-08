708a8c35b70e9f52a5fa81be574b62c20c41d234
Andriy Samilyak <samilyak@gmail.com>
2014-04-08 18:06:28 +0300

    Retaining X days backups

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,11 @@
+directory "#{node["mysql_bckp"]["dump_path"]}" do
+	mode 0777
+end 
+
 template node.mysql_bckp.script_path do
 source "mysql_bckp.erb"
 	mode 0755
   action :create
 end
+
+execute "find #{node["mysql_bckp"]["dump_path"]/*.sql.gz} -mtime +#{node["mysql_bckp"]['days_retain']} -exec rm {} \;"
